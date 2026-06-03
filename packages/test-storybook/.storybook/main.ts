558edde9614d7b4636c107d44d0f316f5b629742
aubquinn <aubreyfo@gmail.com>
2026-06-03 17:35:29 +0000

    fix(storybook): add stories glob for monorepo packages/test-components

--- packages/test-storybook/.storybook/main.ts
+++ packages/test-storybook/.storybook/main.ts
@@ -1,1 +1,23 @@
+import { dirname, join } from "path";
 import '@contrast-lens/storybook-addon';
+
+export const framework = {
+  name: getAbsolutePath("@storybook/react-vite"),
+  options: {}
+};
+
+export const docs = {
+  autodocs: true
+};
+
+export const stories = [
+  join(__dirname, '..', '..', 'test-components', 'src', '**', '*.stories.@(js|jsx|ts|tsx|mdx)')
+];
+
+// Debug output for resolved stories glob
+console.log('STORYBOOK: stories glob ->', stories);
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+export const addons = ["@chromatic-com/storybook"];
